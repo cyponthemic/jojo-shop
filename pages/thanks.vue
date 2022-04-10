c639a386cb9c9dcb329544f9748d5eedf321cac2
Alex Chavet <alex@dotdev.com.au>
2022-04-10 21:13:37 +1000

    feat: fix thanks page

---
+++ pages/thanks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="relative py-16 overflow-hidden">
+      <div class="relative px-4 sm:px-6 lg:px-8">
+        <div class="text-lg max-w-prose mx-auto">
+          <h1>
+            <span
+              class="block text-base text-center text-indigo-500 font-semibold tracking-wide uppercase"
+              >Thank you</span
+            >
+            <span
+              class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl"
+              >We received your enquiry
+            </span>
+          </h1>
+          <p class="mt-8 text-xl text-gray-500 leading-8">
+            We’ll be in touch. Let the saucy times roll...
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
